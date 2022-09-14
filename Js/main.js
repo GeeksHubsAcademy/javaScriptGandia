fb0166640d44bb1a989cc17fa8e69be9f151a68b
Dave86dev <davidochando1986@gmail.com>
2022-09-14 13:13:39 +0200

    Empezando con la teoría de JavaScript

---
+++ Js/main.js
@@ -0,0 +1,61 @@
+
+// console.log("Hola mundo");
+
+// // Esto es un comentario de una línea
+
+// /* Soy un comentario multilinea 
+//  desarrollado en Urbalab en Gandía.*/
+
+//  let nombre = "Adrià";
+
+//  console.log("estoy en la línea 11 y nombre vale ", nombre);
+
+//  nombre = "Cristian";
+
+//  console.log("estoy ahora en la linea 13 y nombre vale ", nombre);
+
+// let nombre2 = "Israel";
+// let bebida = "Agua";
+
+// console.log("A ", nombre2 , " le gusta beber ", bebida);
+
+// let numero1 = 8;
+
+// let nombre = "Toni";
+
+// let interruptor = true;
+
+// let tresPalabrasJuntas = "soy un texto escrito en camelCase";
+
+// let apellido;
+
+// let vacia = "";
+
+// function Sumar (primerNumero, segundoNumero) {
+
+//     let resultado = primerNumero + segundoNumero;
+
+//     return resultado;l
+// };
+
+// let zapato = 3;
+
+// let peluche = 10;
+
+// console.log(Sumar(zapato, peluche));
+// console.log(Sumar(666, 5));
+// console.log(Sumar(2,3));
+// console.log(Sumar(9,8));
+
+function Modulo (a,b) {
+    let resultadoModulo = a % b;
+    return resultadoModulo;
+}
+
+let primero = 500;
+
+let segundo = 300;
+
+console.log(Modulo(primero,segundo));
+
+let mensaje = "hola soy un mensaje de texto, no se muy bien que decir.... hahahahah";
